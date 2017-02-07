346ce04b42cf001085ac80f08b9cabc0f1bde2fb
solucionesjyd <solucionesjyd@live.com>
2017-02-07 09:18:15 -0600

    Add files via upload

---
+++ formulario_clientes.php
@@ -0,0 +1,49 @@
+<?php include '../extend/header.php'; ?>
+
+<div class="row">
+  <div class="col s12">
+    <div class="card">
+      <div class="card-content">
+        <span class="card-title">Alta de clientes</span>
+        <form class="form" action="ins_clientes.php" method="post" autocomplete=off >
+          <div class="input-field">
+            <input type="text" name="nombre"  title="Solo letras" pattern="[A-Z/s ]+"  id="nombre" onblur="may(this.value, this.id)"  >
+            <label for="nombre">Nombre</label>
+          </div>
+          <div class="input-field">
+            <input type="text" name="direccion"    id="direccion" onblur="may(this.value, this.id)"  >
+            <label for="direccion">Dirección</label>
+          </div>
+          <div class="input-field">
+            <input type="text" name="telefono"   id="telefono"  >
+            <label for="telefono">Telefono</label>
+          </div>
+          <div class="input-field">
+            <input type="email" name="email"   id="email"   >
+            <label for="email">Correo</label>
+          </div>
+          <button type="submit" class="btn" >Guardar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col s12">
+    <nav class="brown lighten-3" >
+      <div class="nav-wrapper">
+        <div class="input-field">
+          <input type="search"   id="buscar" autocomplete="off"  >
+          <label for="buscar"><i class="material-icons" >search</i></label>
+          <i class="material-icons" >close</i>
+        </div>
+      </div>
+    </nav>
+  </div>
+</div>
+
+<?php include '../extend/scripts.php'; ?>
+
+</body>
+</html>
